8034b43ccc079ac929b79fb4bc27a4acb4a75278
ruhulcse <ruhul.cse7862@gmail.com>
2023-05-23 22:17:25 +0600

    modified file

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,3 @@
 const users = require("../routes/userRoute");
-const musics = require("../routes/musicRoute");
-const plans = require("../routes/planRoutes");
-const payment = require("../routes/paymentRoute");
 
-module.exports = [users, musics, plans, payment];
+module.exports = [users];
--- routes/musicRoute.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const { musicList } = require("../controllers/musicController");
-const router = express.Router();
-
-router.get("/get_music", musicList);
-module.exports = router;
